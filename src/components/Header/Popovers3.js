00daa7c6e13f6165793e142454f30526d560087c
hashmath3 <hashmathbasha@gmail.com>
2022-11-29 13:58:30 +0530

    port

--- src/components/Header/Popovers3.js
+++ src/components/Header/Popovers3.js
@@ -11,13 +11,16 @@ function Popovers3() {
   <button class="dropbtn"> <div className='down'>
  <span> Portfolio</span> <KeyboardArrowDownIcon sx={{color:"black"}} /></div></button>
   <div class="dropdown-content dropdown-content3">
-   
-<Typography sx={{ p: 1 }}>Grid View</Typography>
-<Typography sx={{ p: 1 }}>List View</Typography>
-<Typography sx={{ p: 1 }}>Slider View</Typography>
-<Typography sx={{ p: 1 }}>Single Project</Typography>
+  <div className='pop-row2'>
+<div>
+ <span className='drop-item'>Grid View</span>
+ <span className='drop-item'>List View</span>
+ <span className='drop-item'>Slider View</span>
+ <span className='drop-item'>Single Project</span>
   </div>
 </div>
+</div>
+  </div>
   </div>
   )
 }
